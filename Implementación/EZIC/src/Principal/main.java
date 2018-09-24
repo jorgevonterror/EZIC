a4a9f6c353c319ccb79da500acf6c352580818a0
jorgevonterror <jorge7373.jagg@gmail.com>
2018-09-24 10:58:43 -0500

    Se le dio la primera funcionalidad al formulario principal para que se muestre en pantalla

--- Implementación/EZIC/src/Principal/main.java
+++ Implementación/EZIC/src/Principal/main.java
@@ -15,7 +15,8 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Home h = new Home();
+       h.show();
     }
     
 }
